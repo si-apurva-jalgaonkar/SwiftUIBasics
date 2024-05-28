4572022fd8b416fd127b0b2a79f2b1c6dbc5053c
Apurva Jalgaonkar <apurva.jalgaonkar@sportzinteractive.net>
2024-05-28 16:31:14 +0530

    Initial Commit

---
+++ SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by Apurva Jalgaonkar on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
